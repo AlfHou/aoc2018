cadbef08ad9956ba9a8790154c379ddf4657f6b9
Alf Sebastian Houge <mr.coolsebastian@hotmail.com>
2018-12-01 12:49:27 +0100

    Both challenges in day 1 complete

--- day1/day1.c
+++ day1/day1.c
@@ -1,10 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
+int findFrequency(void);
+int findRepeatedFrequency(void);
+int countLines(FILE *f);
 
 int main(void) {
+    printf("%d\n", findFrequency());
+    printf("%d\n", findRepeatedFrequency());
+}
+
+int findFrequency(void) {
     int frequency = 0;
     FILE *inputFile;
     inputFile = fopen("./input.txt", "r");
-    
 
+    char line[100];
+    while (fgets(line, 100, inputFile)) {
+        int num = atoi(line);
+        frequency += num;
+    }
     fclose(inputFile);
+    return frequency;
+}
+
+int findRepeatedFrequency(void) {
+    int frequency = 0;
+
+    FILE *inputFile;
+    inputFile = fopen("./input.txt", "r");
+
+    int log[200000];
+    char line[100];
+
+    int counter = 0;
+    while (counter < 200000) {
+        rewind(inputFile);
+        while (fgets(line, 100, inputFile)) {
+            log[counter] = frequency;
+            int num = atoi(line);
+            frequency += num;
+            //Look through the log and look for duplicates, if duplicate found, return frequency
+            int i;
+            for (i = counter +1; i >= 0; i--) {
+                if (frequency == log[i]) {
+                    fclose(inputFile);
+                    return frequency;
+                }
+            }
+        counter++;
+        }
+    }
+    fclose(inputFile);
+    return 20;
 }
