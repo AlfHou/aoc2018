441622fcd6a2c97ec21fd7d1f59c636e5a8ed8d6
Alf Sebastian Houge <mr.coolsebastian@hotmail.com>
2018-12-04 01:51:24 +0100

    Solve challenge 1

--- day3/day3.c
+++ day3/day3.c
@@ -3,39 +3,64 @@
 #include <string.h>
 
 int main(void) {
-    char *fabric[1000][1000];
+    char fabric[1000][1000];
     FILE *file = fopen("./input.txt", "r");
 
-    //Her lager jeg en array med størrelse 256 som er
-    //deklarert til å inneholde char
+    //Fill the array with unclaimed spaces
+    unsigned long i;
+    for (i = 0; i < sizeof(fabric)/sizeof(fabric[0]); i++) {
+        unsigned long j;
+        for (j = 0; j < sizeof(fabric[0]); j++) {
+            fabric[i][j] = '.';
+        }    
+    } 
+
+
     char line[256];
     while (fgets(line, sizeof(line), file)) {
-        //Lager en char peker som peker til adressen
-        //starten av line
         char *linePointer = line;
-        strsep(&linePointer, " ,x");
-        strsep(&linePointer, " ,x");
-        char *xCoordinate = strsep(&linePointer, " ,x:");
-        char *yCoordinate = strsep(&linePointer, " ,:x");
-        //This is a little hard coded because the
-        //strsep function doesnt allow multi-char delimiter
-        strsep(&linePointer, ": ,:x");
-        char *width = strsep(&linePointer, " ,:x");
-        char *height = strsep(&linePointer, " ,:x");
-        
+        strsep(&linePointer, " ,x:");
+        strsep(&linePointer, " ,x:");
 
-        //endre elementet fra fabric[yCoordinate][xCoordinate] 
-        //til og med fabric[yCoordinate + height][xCoordinate + widht]
-        //til C (for counted) hvis elementet som allerede er der
-        // == T eller til T (for taken) hvis ingenting er der fra før
+        char *xCoordinateString = strsep(&linePointer, " ,x:");
+        long xCoordinate = strtol(xCoordinateString, NULL, 10);
 
-        //Gå gjennom fabric og tell antall ganger den finner C
-        
-        
+        char *yCoordinateString = strsep(&linePointer, " ,:x");
+        long yCoordinate = strtol(yCoordinateString, NULL, 10);
 
+        strsep(&linePointer, " ,:x");
 
+        char *widthString = strsep(&linePointer, " ,:x");
+        long width = strtol(widthString, NULL, 10);
+
+        char *heightString = strsep(&linePointer, " ,:x");
+        long height = strtol(heightString, NULL, 10);
+
+        long x;
+        for (x = xCoordinate; x < xCoordinate + width; x++) {
+            long y;
+            for (y = yCoordinate; y < yCoordinate + height; y++) {
+                if (fabric[x][y] == 'T') {
+                    fabric[x][y] = 'C';
+                } else if (fabric[x][y] != 'T' && fabric[x][y] != 'C') {
+                    fabric[x][y] = 'T';
+                }
+            }
+        }
     }
-
+    
+    int fabricCounter = 0;
+    unsigned long x;
+        for (x = 0; x < sizeof(fabric)/sizeof(fabric[0]); x++) {
+            unsigned long y;
+            for (y = 0; y < sizeof(fabric[0]); y++) {
+                if (fabric[x][y] == 'C') {
+                    fabricCounter++;
+                }
+            }
+        } 
+    printf("Overlapping claimed fabric: %d\n", fabricCounter);
+    
     fclose(file);
     exit(EXIT_SUCCESS);
 }
