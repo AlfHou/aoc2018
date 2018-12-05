2e2de35177481accfb13887f0ca00a8c49fe753d
Alf Sebastian Houge <mr.coolsebastian@hotmail.com>
2018-12-05 20:13:40 +0100

    Add entries from input.txt to array

--- day4/day4.c
+++ day4/day4.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void) {
     FILE *file;
     file = fopen("./input.txt", "r");
+
     char line[256];
+    int lineCounter = 0;
+    while(fgets(line, sizeof(line), file)) {
+        lineCounter++;
+    }
+    rewind(file);
+    
+    char entries[lineCounter][256];
     int counter = 0;
     while(fgets(line, sizeof(line), file)) {
-        
+        strcpy(entries[counter], line);
+        counter++;
     }
+    
     fclose(file);
     exit(EXIT_SUCCESS);
 }
